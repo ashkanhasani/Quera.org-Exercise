f712856c43e9b82526f6f8dd9ed7accbb8030664
ashkandma <ashkanhasani31@gmail.com>
2024-01-24 06:54:24 +0330

    do successfully

--- wonder code/source.py
+++ wonder code/source.py
@@ -1,2 +1,20 @@
 class Foo:
-    pass
+    def __init__(self):
+        self._x = 0
+
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, value):
+        if isinstance(value, int):
+            if value >= 0:
+                # If the number is non-negative, store the two digits on the right side
+                self._x = value % 100
+            else:
+                # If the number is negative, set x to -1
+                self._x = -1
+        else:
+            raise ValueError("Value must be of type int")
+
